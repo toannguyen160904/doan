a42c1e0fa401ca5962f48b5afafe93423b7d4920
3305_Nguyễn lê anh toàn <toannguyen160904@gmail.com>
2025-04-21 16:32:35 +0000

    Revert "Merge branch 'thien' into 'thien1'"
    This reverts merge request !6

---
+++ doan/Migrations/20250412060123_RemoveNextReviewDateFromVocabularyAndAddToProgress.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace doan.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveNextReviewDateFromVocabularyAndAddToProgress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tuvung_Baihoc_LessonId",
+                table: "tuvung");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Tuvung",
+                table: "tuvung",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhatAm",
+                table: "tuvung",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nghia",
+                table: "tuvung",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LessonId",
+                table: "tuvung",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HanTu",
+                table: "tuvung",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AmHan",
+                table: "tuvung",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "NextReviewDate",
+                table: "tuvung",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tuvung_Baihoc_LessonId",
+                table: "tuvung",
+                column: "LessonId",
+                principalTable: "Baihoc",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tuvung_Baihoc_LessonId",
+                table: "tuvung");
+
+            migrationBuilder.DropColumn(
+                name: "NextReviewDate",
+                table: "tuvung");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Tuvung",
+                table: "tuvung",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhatAm",
+                table: "tuvung",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nghia",
+                table: "tuvung",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LessonId",
+                table: "tuvung",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HanTu",
+                table: "tuvung",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AmHan",
+                table: "tuvung",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tuvung_Baihoc_LessonId",
+                table: "tuvung",
+                column: "LessonId",
+                principalTable: "Baihoc",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
